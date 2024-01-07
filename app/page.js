b68ff2f772baff3c2b14117c159ffa777971806a
yejinzhan <jinzhan.ye@foxmail.com>
2024-01-07 15:34:52 +0800

    update

--- app/page.js
+++ app/page.js
@@ -1,6 +1,14 @@
+"use client";
+
+import { unstable_noStore as noStore } from 'next/cache';
 import Image from 'next/image'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default function Home() {
+  noStore();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
